bfd1b0a013e3d997b3891cb0ddd4a4aeb8b84995
NickPro69 <k.gorzelski29@gmail.com>
2021-12-04 21:58:57 +0100

    day four

---
+++ day4/PartOne.ts
@@ -0,0 +1,60 @@
+import { readFileSync } from "fs"
+
+const input = readFileSync("./Day4/Input.txt").toString().split("\r\n")
+
+const numbers = input[0].split(",")
+let completeBoards: string[][][] = []
+let boardCount = 0
+for (let i = 2; i < input.length; i += 6) {
+    completeBoards.push([])
+    for (let j = 0; j <= 4; j++) {
+        completeBoards[boardCount].push(input[i + j].split(' ').filter((data) => data != ''))
+    }
+    boardCount ++
+}
+
+let boards: string[][][] = []
+let columns: number[][] = []
+let rows: number[][] = []
+
+for (let i = 0; i < completeBoards.length; i++) {
+    columns.push([])
+    rows.push([])
+    for (let j = 0; j <= 4; j++) {
+        columns[i][j] = 0
+        rows[i][j] = 0
+    }
+}
+
+loop: for (const number of numbers) {
+    for (let i = 0; i < completeBoards.length; i++) {
+        if (number == numbers[0]) boards.push([])
+        for (let j = 0; j <= 4; j++) {
+            if (number == numbers[0]) boards[i].push([])
+            for (let k = 0; k <= 4; k++) {
+                if (number == completeBoards[i][j][k]) {
+                    boards[i][j][k] = number
+                    rows[i][j] ++
+                    columns[i][k] ++
+                    if (rows[i][j] == 5 || columns[i][k] == 5) {
+                        boardFound(Number(number), i)
+                        break loop
+                    }
+                }
+            }
+        }
+    }
+}
+
+function boardFound(number: number, i: number) {
+    const winningBoardFull = completeBoards[i]
+    const winningBoard = boards[i]
+
+    let totalNotFound = 0
+    for (let i = 0; i <= 4; i++) {
+        for (let j = 0; j <= 4; j++) {
+            if (winningBoard[i][j] == undefined) totalNotFound += Number(winningBoardFull[i][j])
+        }
+    }
+    console.log('Part One:', totalNotFound * number)
+}
